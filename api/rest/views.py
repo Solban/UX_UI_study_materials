f250f944dcec138d516063f784f48efd50e5dae9
Joosep L <hansjooseplahe@gmail.com>
2017-05-05 19:16:56 +0300

    Category - Answer - Example available

--- api/rest/views.py
+++ api/rest/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets, generics, filters
 
-# Create your views here.
+from .serializers import *
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
+
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class ExampleViewSet(viewsets.ModelViewSet):
+    serializer_class = ExampleSerializer
+    queryset = Example.objects.all()
+    lookup_field = "category__name"
