fb91cebfe4414f1be56d2813268445475f5df22e
Sven Kadak <sven.kadak@gmail.com>
2017-05-08 15:22:37 +0300

    Create examples JSON template

---
+++ app/src/examples.js
@@ -0,0 +1,116 @@
+/*
+Template:
+
+
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+
+*/
+
+
+module.exports = {
+  'Button': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Dialog': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Error page': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Search': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Navigation': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Form': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Chat': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Notification': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Map': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'List': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ],
+  'Color': [
+    {
+      img1: '',
+      img2: '',
+      do: '',
+      dont: '',
+      description: ''
+    }
+  ]
+};
